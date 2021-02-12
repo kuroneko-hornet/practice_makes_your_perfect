10c3e49dc31755f6f54b6af16b910f92c096868f
Hiromichi <s165628s@st.go.tuat.ac.jp>
2021-02-12 17:41:50 +0900

    add DIETING/TRAINING columns

--- www/minitwitter.php
+++ www/minitwitter.php
@@ -7,10 +7,11 @@
     function myesc($str) {
         return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
     }
+
     // 新規メッセージがあれば
     if ( isset($_REQUEST['new_message']) && $_REQUEST['new_message'] != '' ){
         // 先頭30バイトを取得し、改行コードを <br> に置換
-        $new_message = mb_substr($_REQUEST['new_message'], 0, 30);
+        $new_message = mb_substr($_REQUEST['new_message'], 0, 1024);
         $new_message = str_replace(["\r\n", "\r", "\n"], '<br>', $new_message);
 
         // 新規メッセージを、配列の先頭にセット
@@ -22,9 +23,9 @@
         $fp = fopen("tweets.txt", "r");
         while (( $line = fgets($fp)) !== false ) {
             array_push($lines, $line);
-            if ( count($lines) > 4 ){
-                break;
-            }
+            // if ( count($lines) > 4 ){
+            //     break;
+            // }
         }
         fclose($fp);
 
@@ -40,35 +41,44 @@
     // [year, month, day, hour, minute]
     $date_keys = ['year', 'month', 'day', 'hour', 'minute'];
     $today = array_combine($date_keys, explode('/', date("Y/m/d/H/i")));
-    $selected_date = [
-        'year'=>$_POST['view_year'] ?: $today['year'],
-        'month'=>$_POST['view_month']  ?: $today['month'],
-        'day'=>$_POST['view_day'] ?: $today['day']
+    $selected_date_diet = [
+        'year'=>$_POST['view_dyear'] ?: $today['year'],
+        'month'=>$_POST['view_dmonth']  ?: $today['month'],
+        'day'=>$_POST['view_dday'] ?: $today['day']
     ];
+    $selected_date_training = [
+        'year'=>$_POST['view_tyear'] ?: $today['year'],
+        'month'=>$_POST['view_tmonth']  ?: $today['month'],
+        'day'=>$_POST['view_tday'] ?: $today['day']
+    ];
+    print_r($selected_date_diet);
+    echo '<br>';
+    print_r($selected_date_training);
+    echo '<br>';
 
     // 検索フォーム
-    $date_list = ['year'=>range(2021, 2019, -1), 'month'=>range(1, 12), 'day'=>range(1, 31)];
-    foreach(['year', 'month', 'day'] as $date_cls){
-        foreach($date_list[$date_cls] as $val){
-            $date_val = str_pad($val, 2, 0, STR_PAD_LEFT);
-            $date_list[$date_cls] .= $date_val === $selected_date[$date_cls] ? '<option selected ' : '<option ';
-            $date_list[$date_cls] .= 'value='.$date_val;
-            // $date_list[$date_cls] .= $date_cls === 'day' ? '>' : '/>';
-            $date_list[$date_cls] .= ">$val</option>";
+    $date_list = [
+        ['year'=>range(2021, 2019, -1), 'month'=>range(1, 12), 'day'=>range(1, 31)],
+        ['year'=>range(2021, 2019, -1), 'month'=>range(1, 12), 'day'=>range(1, 31)]
+    ];
+    foreach([$selected_date_diet, $selected_date_training] as $i => $selected_date){
+        foreach(['year', 'month', 'day'] as $date_cls){
+            foreach($date_list[$i][$date_cls] as $val){
+                $date_val = str_pad($val, 2, 0, STR_PAD_LEFT);
+                $date_list[$i][$date_cls] .= $date_val === $selected_date[$date_cls] ? '<option selected ' : '<option ';
+                $date_list[$i][$date_cls] .= 'value='.$date_val;
+                // $date_list[$date_cls] .= $date_cls === 'day' ? '>' : '/>';
+                $date_list[$i][$date_cls] .= ">$val</option>";
+            }
         }
     }
-    $view_name = [
-        [" 年 ", 'view_year'],
-        [' 月 ', 'view_month'],
-        [' 日 ', 'view_day']
-    ];
 
     echo '\$_POST: ';
     print_r($_POST);
     echo '<br>';
     print_r($today);
     echo '<br>';
-    print_r($selected_date);
+    // print_r($date_list);
     echo '<br>';
 
 ?>
@@ -79,19 +89,41 @@
 </head>
 <body>
     <h1>Practice Makes Your Perfect.</h1>
-    <h2>Have you eaten anything?</h2>
 
-    <h2>Your effort</h2>
-    <form action="<?= $_SERVER["SCRIPT_NAME"]?>?<?= microtime() ?>" method="post">
-        <?php foreach(['year', 'month', 'day'] as $i => $date_cls) {
-            echo '<select name='.$view_name[$i][1].'>'.$date_list[$date_cls].'</select>'.$view_name[$i][0];
-        } ?>
-    <input type='submit' value='検索' /></form>
 
-    <form action="<?= $_SERVER["SCRIPT_NAME"]?>?<?= microtime() ?>" method="POST">
-        メッセージ: <textarea name="new_message" cols=30 rows=3></textarea>
-        <input type="submit" value="投稿">
-    </form>
+    <h2>Your effort of DIETING</h2>
+        
+        <h3>Record</h3>
+
+        <h3>Review</h3>
+        <form action="<?= $_SERVER["SCRIPT_NAME"]?>?<?= microtime() ?>" method="post">
+            <?php 
+                echo 'date: ';
+                foreach(['year', 'month', 'day'] as $i => $date_cls) {
+                echo '<select name=view_d'.$date_cls.'>'.$date_list[0][$date_cls].'</select> ';
+                echo $date_cls === 'day' ? '' : '/ ';
+            } ?>
+        <input type='submit' value='search' /></form>
+
+
+    <h2>Your effort of TRAINING</h2>
+        
+        <h3>Record</h3>
+        <form action="<?= $_SERVER["SCRIPT_NAME"]?>?<?= microtime() ?>" method="POST">
+            メッセージ: <textarea name="new_message" cols=30 rows=3></textarea>
+            <input type="submit" value="registar">
+        </form>
+
+        <h3>Review</h3>
+        <form action="<?= $_SERVER["SCRIPT_NAME"]?>?<?= microtime() ?>" method="post">
+            <?php 
+                echo 'date: ';
+                foreach(['year', 'month', 'day'] as $i => $date_cls) {
+                echo '<select name=view_t'.$date_cls.'>'.$date_list[1][$date_cls].'</select> ';
+                echo $date_cls === 'day' ? '' : '/ ';
+            } ?>
+        <input type='submit' value='search' /></form>
+
 
     <?php
     $fp = fopen("tweets.txt", "r");
@@ -99,12 +131,12 @@
         $cols = explode(',', $line, 2);
         $date = $cols[0];
         $message = str_replace('<br>', "\n", $cols[1]);
-        if (strpos($date, implode('/', $selected_date)) !== false) {
-            echo '日時: <?= '.myesc($date).' ?><br>メッセージ: <?= '.nl2br(myesc($message)).' ?>';
+        if (strpos($date, implode('/', $selected_date_diet)) !== false) {
+            echo '日時: '.$date.'<br>メッセージ: '.nl2br(myesc($message));
+            // echo '日時: <?= '.myesc($date).' 
+            ## <br>メッセージ: <?= '.nl2br(myesc($message)).'';
         }
     }
     ?>
 </body>
 </html>
-
-
